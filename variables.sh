f443587fcf1831303aa5d374781363e43426285e
stockerts <108150421+stockerts@users.noreply.github.com>
2024-09-24 20:52:03 -0700

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,4 +1,5 @@
-xc_auth_value="5k8seiuqQ23KdfeB6H"
-protocol="http"
-domain="stock-demo-api.cloud.myf5demo.com"
-iterations="3"
+xc_auth_value="5k8seiuqQ23KdfeB6H" # Value used within XC Service Policy (ACL) 
+protocol="http" # http or https
+domain="stock-demo-api.cloud.myf5demo.com" 
+iterations="10" # Number of times to run requests
+delay="1" # Time between requests in seconds
