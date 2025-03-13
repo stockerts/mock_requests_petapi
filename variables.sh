ba500336127b2b2ccf0c037df9aca450fd7894f0
stockerts <108150421+stockerts@users.noreply.github.com>
2025-03-12 21:41:13 -0700

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 # xc_auth_value="removed" # Value used within XC Service Policy (ACL) ~ moved to github secrets
-protocol="http" # http or https
+protocol="https" # http or https
 domain="stock-demo-api.cloud.myf5demo.com" 
 iterations="25" # Number of times to run requests
 delay="1" # Time between requests in seconds
